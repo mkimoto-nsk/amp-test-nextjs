dc5c3eac1b1c13d40a1e17c8da74af19c42acc7f
mkimoto-nsk <m.kimoto@nskint.co.jp>
2025-02-10 15:29:26 +0900

    ヘッダーにログアウトボタンを追加した。

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState }  from 'react';
 import './header.css'; // Headerコンポーネントに対応するCSSファイルをインポート
 import  Link  from 'next/link';
-import { FetchUserAttributesOutput, fetchUserAttributes } from 'aws-amplify/auth';
+import { FetchUserAttributesOutput, fetchUserAttributes } from 'aws-amplify/auth'; 
+import { Authenticator } from "@aws-amplify/ui-react";
+import {customComponents, formFields} from "../components/custom_sign_in_up";
 
 const Header: React.FC = () => {
     // ログイン中のユーザ情報を取得して表示する
     const [attr, setAttrResult] = useState<FetchUserAttributesOutput>();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // メニューの開閉状態
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen); // メニューの状態を切り替え
+    };
+
     const getCurrentUserAsync = async () => {
     const result = await fetchUserAttributes();
         // console.log(result);
@@ -17,14 +25,36 @@ const Header: React.FC = () => {
       }, []);
 
     return (
-        <header className="header">
-        <Link href="/" className="logo">
-            Amazon Amplifyで作るWebアプリ
-        </Link>
-        <div user-info>
-            ログイン中：{attr?.nickname}
-        </div>
-        </header>
+        <Authenticator formFields={formFields} components={customComponents}>
+        {({ signOut, user }) => (
+            <header className="header">
+            <Link href="/" className="logo">
+                Amazon Amplifyで作るWebアプリ
+            </Link>
+
+            <div className="user-info">
+                {attr?.nickname} 
+                    <div className={`arrow-down ${isMenuOpen ? 'rotate-up' : ''}`}
+                        onClick={toggleMenu}
+                    >
+
+                    </div>
+
+            </div>
+
+            {/* メニュー */}
+            {isMenuOpen && (
+                <div className="dropdown-menu">
+                    <ul>
+                        <li><Link href="/option">オプション(未実装)</Link></li>
+                        <li><button onClick={signOut}>ログアウト</button></li>
+                    </ul>
+                </div>
+            )}
+
+            </header>
+        )}
+        </Authenticator>
     );
 };
 
